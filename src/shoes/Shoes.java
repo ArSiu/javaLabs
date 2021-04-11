5c5cf7369f625583bd22c633f07f7d90830dd87d
ArSiu <arsvikh@gmail.com>
2021-04-11 14:21:02 +0300

    divided the code into packages and took main() to a separate class Main.java

--- src/shoes/Shoes.java
+++ src/shoes/Shoes.java
@@ -1,3 +1,5 @@
+package shoes;
+
 public class Shoes{
     private static int popularity;
 
@@ -153,20 +155,5 @@ public class Shoes{
     }
 
 
-    public static void main(String[] args) {
-	    Shoes shoe1 = new Shoes();
-        Shoes shoe2 = new Shoes("Abibibas", 100, 7, "Brawn");
-        Shoes shoe3 = new Shoes("Nike", 150, 8, "White", "AIR", "China",
-                "summer 90s", "cotton", true, 10, 1000);
 
-        System.out.println(shoe3.toString());
-
-        shoe1.resetValues("Nike", 500, 7, "Red", "AIR", "USA",
-                "summer 80s", "Polyester", true, 8, 10000);
-
-        System.out.println(shoe1.toString());
-
-        shoe2.printPopularity();
-        Shoes.printStaticPopularity();
-    }
 }
