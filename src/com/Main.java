9749d37cab6393e45080a9138042aaa1e132dff0
ArSiu <33807305+ArSiu@users.noreply.github.com>
2021-04-14 11:42:38 +0300

    change name of example objects

--- src/com/Main.java
+++ src/com/Main.java
@@ -3,19 +3,19 @@ import shoes.Shoes;
 
 public class Main {
     public static void main(String[] args) {
-        Shoes shoe1 = new Shoes();
-        Shoes shoe2 = new Shoes("Abibibas", 100, 7, "Brawn");
-        Shoes shoe3 = new Shoes("Nike", 150, 8, "White", "AIR", "China",
+        Shoes shoe = new Shoes();
+        Shoes abibas = new Shoes("Abibibas", 100, 7, "Brawn");
+        Shoes nike = new Shoes("Nike", 150, 8, "White", "AIR", "China",
                 "summer 90s", "cotton", true, 10, 1000);
 
-        System.out.println(shoe3.toString());
+        System.out.println(nike.toString());
 
-        shoe1.resetValues("Nike", 500, 7, "Red", "AIR", "USA",
+        shoe.resetValues("Nike", 500, 7, "Red", "AIR", "USA",
                 "summer 80s", "Polyester", true, 8, 10000);
 
-        System.out.println(shoe1.toString());
+        System.out.println(shoe.toString());
 
-        shoe2.printPopularity();
+        abibas.printPopularity();
         Shoes.printStaticPopularity();
     }
 }
